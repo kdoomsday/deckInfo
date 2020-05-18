e0bbc98bc53f8beadefbf64c04d3e53ab40261ce
Eduardo Barrientos <doomsday@pop-os.localdomain>
2020-05-18 09:03:31 -0400

    Eliminar código comentado

--- core/src/main/scala/ebarrientos/deckStats/load/StoringLoader.scala
+++ core/src/main/scala/ebarrientos/deckStats/load/StoringLoader.scala
@@ -20,18 +20,6 @@ trait StoringLoader extends CardLoader {
   /** Almacenar una carta en la instancia */
   protected def store(c: Card): Task[Unit]
 
-  // Intentar obtener la carta. Si no se puede se pregunta al helper
-  // En caso de que se consiga con el helper, se almacena y se devuelve
-  // final def card(name: String): Task[Option[Card]] = {
-  //   retrieve(name) match {
-  //     case oC @ Some(_) =>
-  //       println(s"StoringLoader found $name")
-  //       IO.succeed(oC)
-  //     case None         =>
-  //       println(s"Did not find $name, going to helper")
-  //       helper.card(name).map(oc => { oc.map(store _); oc })
-  //   }
-  // }
   final def card(name: String): Task[Option[Card]] = {
     for {
       ocard    <- retrieve(name)
