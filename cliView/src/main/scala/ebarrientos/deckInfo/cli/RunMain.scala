8c0fdefb7c10eeb0ecc5fd56d8ebc9f9d3fbed4e
Eduardo Barrientos <doomsday@pop-os.localdomain>
2021-01-13 16:29:10 -0400

    Usar ExitCode que ahora hace falta

--- cliView/src/main/scala/ebarrientos/deckInfo/cli/RunMain.scala
+++ cliView/src/main/scala/ebarrientos/deckInfo/cli/RunMain.scala
@@ -1,12 +1,13 @@
 package ebarrientos.deckInfo.cli
+
 import ebarrientos.deckStats.math.Calc
 import ebarrientos.deckStats.load.XMLDeckLoader
 import ebarrientos.deckStats.load.CardLoader
-import ebarrientos.deckStats.load.{ DeckLoader, MtgJsonLoader }
+import ebarrientos.deckStats.load.{DeckLoader, MtgJsonLoader}
 import scala.io.Source
 import ebarrientos.deckStats.basics._
 
-import zio.{ App, Task }
+import zio.{App, Task}
 import zio.console._
 import zio.ZIO
 import ebarrientos.deckStats.load.H2DBDoobieLoader
@@ -18,26 +19,27 @@ import ebarrientos.deckStats.config.CoreConfig
 import scala.concurrent.ExecutionContext
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
+import zio.ExitCode
 
 /** Entrypoint to the cli interface */
 object RunMain extends App {
 
   final def run(args: List[String]) =
-    logic(args).fold(_ => 1, _ => 0)
+    logic(args).fold(_ => ExitCode(1), _ => ExitCode(0))
 
   def logic(args: List[String]) =
     for {
-      path       <- path(args)
-      _          <- putStrLn(s"Path = $path")
-      config     <- ZIO.fromEither(ConfigSource.default.load[CoreConfig])
-      _          <- putStrLn("Config loaded...")
+      path      <- path(args)
+      _         <- putStrLn(s"Path = $path")
+      config    <- ZIO.fromEither(ConfigSource.default.load[CoreConfig])
+      _         <- putStrLn("Config loaded...")
       // cardLoader  = new MtgJsonLoader(Source.fromInputStream(getClass.getClassLoader.getResourceAsStream("AllCards.json")).mkString)
-      ec          = ExecutionContext.fromExecutorService(Executors.newCachedThreadPool())
-      cardLoader  = new H2DBDoobieLoader(MagicIOLoader, config, ec)
-      deckLoader  = new XMLDeckLoader(path, cardLoader)
-      _          <- putStrLn("Loading deck...\n")
-      deck       <- deckLoader.load()
-      _          <- printDeck(deck)
+      ec         = ExecutionContext.fromExecutorService(Executors.newCachedThreadPool())
+      cardLoader = new H2DBDoobieLoader(MagicIOLoader, config, ec)
+      deckLoader = new XMLDeckLoader(path, cardLoader)
+      _         <- putStrLn("Loading deck...\n")
+      deck      <- deckLoader.load()
+      _         <- printDeck(deck)
     } yield ()
 
   def path(args: List[String]): Task[String] =
@@ -54,25 +56,28 @@ object RunMain extends App {
   /** Print deck stats to the cli */
   private[this] def printDeck(d: Deck) = {
     for {
-      _      <- putStr("")
-      cards   = Calc.count(d)
-      avgCost = Calc.avgManaCost(d)
+      _ <- putStr("")
+
+      cards       = Calc.count(d)
+      avgCost     = Calc.avgManaCost(d)
       avgNonLands = Calc.avgManaCost(d, c => !c.is(Land))
+
       _ <- putStrLn(s"""|Cards: $cards
                         |Average cost: $avgCost
                         |Average (nonlands): $avgNonLands""".stripMargin)
       _ <- putStrLn("")
 
-      creats = Calc.count(d, c => c.is(Creature))
-      sorcs  = Calc.count(d, c => c.is(Sorcery))
-      insts  = Calc.count(d, c => c.is(Instant))
-      lands  = Calc.count(d, c => c.is(Land))
-      arts   = Calc.count(d, c => c.is(Artifact))
-      enchs  = Calc.count(d, c => c.is(Enchantment))
-      walker = Calc.count(d, c => c.is(Planeswalker))
+      creats        = Calc.count(d, c => c.is(Creature))
+      sorcs         = Calc.count(d, c => c.is(Sorcery))
+      insts         = Calc.count(d, c => c.is(Instant))
+      lands         = Calc.count(d, c => c.is(Land))
+      arts          = Calc.count(d, c => c.is(Artifact))
+      enchs         = Calc.count(d, c => c.is(Enchantment))
+      walker        = Calc.count(d, c => c.is(Planeswalker))
       nonPermanents = sorcs + insts
       permanents    = cards - (lands + nonPermanents)
       nonLands      = permanents + nonPermanents
+
       _ <- putStrLn(s"""|Permanents: $permanents
                         |Non premanents: $nonPermanents
                         |
@@ -87,19 +92,21 @@ object RunMain extends App {
       _ <- putStrLn("")
 
       curve = Calc.manaCurve(d)
-      _ <- printCurve(curve)
-      _ <- putStrLn("")
+      _    <- printCurve(curve)
+      _    <- putStrLn("")
 
       symbols = Calc.manaSymbols(d)
-      _ <- printSymbols(symbols)
-      _ <- putStrLn("")
+      _      <- printSymbols(symbols)
+      _      <- putStrLn("")
     } yield ()
   }
 
-  def printCurve(curve:  Seq[(Int, Int)]) =
+  def printCurve(curve: Seq[(Int, Int)]) =
     // Task.foreach(curve)((cost, amnt) => putStr(s"$cost: ").andThen(putStrLn('#'*amnt)))
-    ZIO.foreach(curve){ case (cost, amnt) => putStrLn(s"$cost: ${"#"*amnt} ($amnt)") }
+    ZIO.foreach(curve) { case (cost, amnt) =>
+      putStrLn(s"$cost: ${"#" * amnt} ($amnt)")
+    }
 
   def printSymbols(symbols: Map[String, Double]) =
-    ZIO.foreach(symbols){ case (symbol, amnt) => putStrLn(s"$symbol: $amnt") }
+    ZIO.foreach_(symbols) { case (symbol, amnt) => putStrLn(s"$symbol: $amnt") }
 }
