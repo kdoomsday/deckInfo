91746fd7e8d55e371ddbaa9525d15da892a59bda
Eduardo Barrientos <doomsday@pop-os.localdomain>
2021-02-20 23:30:57 -0400

    Make calculation more functional

--- core/src/main/scala/ebarrientos/deckStats/queries/DeckCalc.scala
+++ core/src/main/scala/ebarrientos/deckStats/queries/DeckCalc.scala
@@ -1,52 +1,34 @@
 package ebarrientos.deckStats.queries
 
 import ebarrientos.deckStats.math.Calc._
-import ebarrientos.deckStats.basics.Deck
-import ebarrientos.deckStats.basics.Land
-import ebarrientos.deckStats.basics.Creature
-import ebarrientos.deckStats.basics.Instant
-import ebarrientos.deckStats.basics.Sorcery
-import ebarrientos.deckStats.basics.Planeswalker
-import ebarrientos.deckStats.basics.Artifact
-import ebarrientos.deckStats.basics.Enchantment
+import ebarrientos.deckStats.basics._
 
 object DeckCalc {
 
   /** Full calculations
     *
     * @param d The deck
-    * @return [[DeckObject]] with the corresponding values
+    * @return [[DeckObject]] with the calculated values
     */
   def fullCalc(d: Deck): DeckObject = {
+    // Calculate the counts, by grouping and counting
     def counts(): TypeCounts = {
-      // Each card can have more than one type, so we must count individually
-      var lands         = 0
-      var creatures     = 0
-      var instants      = 0
-      var sorceries     = 0
-      var planeswalkers = 0
-      var artifacts     = 0
-      var enchantments  = 0
-
-      d.cards
-        .foreach(c => {
-          if (c.types.contains(Land))         lands += 1
-          if (c.types.contains(Creature))     creatures += 1
-          if (c.types.contains(Instant))      instants += 1
-          if (c.types.contains(Sorcery))      sorceries += 1
-          if (c.types.contains(Planeswalker)) planeswalkers += 1
-          if (c.types.contains(Artifact))     artifacts += 1
-          if (c.types.contains(Enchantment))  enchantments += 1
-        })
+      val mapcount =
+        d.cards
+          .flatMap(_.types)
+          .groupBy(identity)
+          .map { case (ct, s) =>
+            (ct, s.size)
+          }
 
       TypeCounts(
-        lands,
-        creatures,
-        instants,
-        sorceries,
-        planeswalkers,
-        artifacts,
-        enchantments
+        mapcount.getOrElse(Land, 0),
+        mapcount.getOrElse(Creature, 0),
+        mapcount.getOrElse(Instant, 0),
+        mapcount.getOrElse(Sorcery, 0),
+        mapcount.getOrElse(Planeswalker, 0),
+        mapcount.getOrElse(Artifact, 0),
+        mapcount.getOrElse(Enchantment, 0)
       )
     }
 
