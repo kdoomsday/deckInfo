eed97f6e1444ea19bd7c292f51990c3b66800aaf
Eduardo Barrientos <doomsday@pop-os.localdomain>
2021-03-14 23:17:38 -0400

    Fix MagicIOLoader card type parsing

--- core/src/main/scala/ebarrientos/deckStats/load/MagicIOLoader.scala
+++ core/src/main/scala/ebarrientos/deckStats/load/MagicIOLoader.scala
@@ -1,14 +1,19 @@
 package ebarrientos.deckStats.load
 
-import ebarrientos.deckStats.basics.{ Card, Mana }
-import ebarrientos.deckStats.load.utils.{ LoadUtils, URLUtils }
+import ebarrientos.deckStats.basics.{Card, Mana}
+import ebarrientos.deckStats.load.utils.{LoadUtils, URLUtils}
 import org.json4s._
 import org.json4s.native.JsonMethods._
 import zio.IO
 import requests.Response
+import scala.annotation.tailrec
+import ebarrientos.deckStats.basics.CardType
+import ebarrientos.deckStats.basics.Supertype
+import org.slf4j.LoggerFactory
 
 /** Loader para cargar información de api.magicthegathering.io */
 object MagicIOLoader extends CardLoader with LoadUtils with URLUtils {
+  private val log = LoggerFactory.getLogger(getClass())
 
   private[this] val baseUrl = "https://api.magicthegathering.io/v1/cards"
 
@@ -17,10 +22,10 @@ object MagicIOLoader extends CardLoader with LoadUtils with URLUtils {
       case JString(s) => s
       case _          => ""
     }
-    def getInt(value: JValue): Int = value match {
-      case JInt(num) => num.toInt
+    def getInt(value: JValue): Int    = value match {
+      case JInt(num)                              => num.toInt
       case JString(txt) if (txt matches "[0-9]+") => txt.toInt
-      case _         => 0
+      case _                                      => 0
     }
 
     /** In an array of results, find the one that fully matches the expected name.
@@ -28,6 +33,7 @@ object MagicIOLoader extends CardLoader with LoadUtils with URLUtils {
       * searching for 'Wasteland' a result will come in for 'Wasteland Scorpion'
       */
     def findObject(arr: JArray): Option[JValue] = {
+      @tailrec
       def fo(l: List[JValue]): Option[JValue] =
         l match {
           case jobject :: rest =>
@@ -45,10 +51,10 @@ object MagicIOLoader extends CardLoader with LoadUtils with URLUtils {
 
     // Construir la carta a partir del jobject correspondiente ya extraido de la lista
     def cardFromJobject(j: JValue): Card = {
-      import ebarrientos.deckStats.stringParsing.MtgJsonParser.{parseAll, cost}
+      import ebarrientos.deckStats.stringParsing.MtgJsonParser.{cost, parseAll}
 
       val manaCost: Seq[Mana] = parseAll(cost, getStr(j \\ "manaCost")).get
-      val (supertypes, types, subTypes) = parseTypes(getStr(j \\ "type"))
+      val (supertypes, types, subTypes) = parseTypesJson(j)
       Card(
         manaCost,
         name,
@@ -62,17 +68,46 @@ object MagicIOLoader extends CardLoader with LoadUtils with URLUtils {
     }
 
     IO.effect {
-      val cardJsonResponse: Response = requests.get(baseUrl, params=Map("name" -> name), readTimeout=20000, connectTimeout=20000)
+      val cardJsonResponse: Response = requests.get(
+        baseUrl,
+        params = Map("name" -> name),
+        readTimeout = 20000,
+        connectTimeout = 20000
+      )
 
       if (cardJsonResponse.statusCode == 200) {
         (parse(cardJsonResponse.text) \\ "cards") match {
-          case arr @ JArray(_) => findObject(arr).map(cardFromJobject _) // Some(cardFromJobject(jobject))
+          case arr @ JArray(_) =>
+            findObject(arr).map(cardFromJobject)
           case _               => None
         }
       }
       else {
-        throw new Exception(s"Error loading card: Status ${cardJsonResponse.statusCode}")
+        throw new Exception(
+          s"Error loading card: Status ${cardJsonResponse.statusCode}"
+        )
       }
     }
+  }
+
+  /** From json get subtertypes, types and subtypes */
+  def parseTypesJson(
+      cardJson: JValue
+  ): (Set[Supertype], Set[CardType], Set[String]) = {
+
+    val types = for {
+      JString(t) <- (cardJson \ "types")
+    } yield CardType(t)
+
+    val supertypes = for {
+      JString(t) <- (cardJson \ "supertypes")
+    } yield Supertype(t)
+
+    val subtypes = for {
+      JString(t) <- (cardJson \ "subtypes")
+    } yield t
+
+    log.debug(s"""Types = ${types.mkString(", ")}""")
+    (supertypes.toSet, types.toSet, subtypes.toSet)
   }
 }
--- core/src/test/scala/ebarrientos/deckStats/load/MagicIOLoaderTest.scala
+++ core/src/test/scala/ebarrientos/deckStats/load/MagicIOLoaderTest.scala
@@ -1,18 +1,31 @@
 package ebarrientos.deckStats.load
 
 import utest._
+import ebarrientos.deckStats.basics.Creature
+import org.slf4j.LoggerFactory
 
 object MagicIOLoaderTest extends TestSuite {
+  val log = LoggerFactory.getLogger(getClass())
+
   val tests = Tests {
     "Load a card successfully" - {
       val ioDC = MagicIOLoader.card("Dark Confidant")
-      val t = ioDC.map(oc =>  oc match {
-        case Some(dc) => assert(dc.power == 2)
-        case None     => throw new Exception("No card found")
-      })
+      val t    = ioDC.map(oc =>
+        oc match {
+          case Some(dc) => {
+            val typesStr = dc.types.mkString(", ")
+            log.debug(s"Name=${dc.name}. Types=$typesStr")
+
+            assert(dc.power == 2)
+            assert(dc.types.contains(Creature))
+            assert(dc.subtypes contains "Human")
+            assert(dc.subtypes contains "Wizard")
+          }
+          case None     => throw new Exception("No card found")
+        }
+      )
 
       zio.Runtime.default.unsafeRun(t)
     }
   }
 }
-
