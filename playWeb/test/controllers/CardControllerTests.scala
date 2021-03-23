305ec787cded93153e4fe208a29227868c85d1ad
Eduardo Barrientos <doomsday@pop-os.localdomain>
2021-03-22 23:28:33 -0400

    Additions to CardControllerTests

--- playWeb/test/controllers/CardControllerTests.scala
+++ playWeb/test/controllers/CardControllerTests.scala
@@ -7,7 +7,6 @@ import play.api.mvc.MultipartFormData
 import akka.util.ByteString
 import play.api.libs.streams.Accumulator
 import play.api.mvc.Result
-import akka.stream.ActorMaterializer
 import akka.actor.ActorSystem
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
@@ -16,36 +15,47 @@ import play.api.test.Helpers._
 import play.api.libs.Files
 import java.io.File
 import java.nio.file.Paths
+import java.util.UUID
+import play.api.Application
+import play.api.Logger
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.mvc.PlayBodyParsers
+import play.api.mvc.AnyContentAsMultipartFormData
 
 object CardControllerTests extends TestSuite {
-  implicit val actorSystem = ActorSystem()
+  val log = Logger(getClass())
 
-  implicit val materializer =
-    ActorMaterializer() // TODO No se como usar el system wide yet
-  implicit val executor = scala.concurrent.ExecutionContext.global
+  implicit val actorSystem = ActorSystem()
 
   val duration = Duration.create(1, TimeUnit.SECONDS)
 
   val tests = Tests {
 
     test("deckUpload") {
-      val path     = "/home/doomsday/code/deckInfo/playWeb/test/Test.dec"
+      val path     = "test/Test.dec"
       val dataFile = Paths.get(path)
+
+      log.debug(s"Path: ${dataFile.getFileName()} - Size: ${java.nio.file.Files.size(dataFile)}")
+
       val tempFile = Files.SingletonTemporaryFileCreator.create(dataFile)
       val part     = MultipartFormData.FilePart(
         "deck",
         "Test",
-        Some("application/octet-stream"),
+        Some("application/xml"),
         tempFile
       )
       val formData = MultipartFormData(dataParts = Map(), files=Seq(part), badParts = Seq())
+      val anyc = AnyContentAsMultipartFormData(formData)
 
+      // val request = FakeRequest(method = "POST", path = "/deck")
+      //   .withHeaders("Content-Type" -> ("multipart/form-data; boundary=--dummy"))
+      //   .withMultipartFormDataBody(formData)
       val request = FakeRequest(method = "POST", path = "/deck")
-        .withMultipartFormDataBody(formData)
-        .withHeaders("Content-Type" -> "undefined")
+        .withBody(anyc)
+        .withHeaders("Content-Type" -> ("multipart/form-data; boundary=--dummy"))
 
       val controller = new CardController(
-        Helpers.stubControllerComponents(),
+        Helpers.stubControllerComponents(playBodyParsers = PlayBodyParsers()),
         new ZioRunnerDefault()
       )
       val result     = controller.deckStats.apply(request)
