89b3f979f304446781994ff1084538f7be485f31
Eduardo Barrientos <doomsday@pop-os.localdomain>
2021-04-01 16:31:04 -0400

    NaturalDeckLoader

---
+++ core/src/main/scala/ebarrientos/deckStats/load/NaturalDeckLoader.scala
@@ -0,0 +1,58 @@
+package ebarrientos.deckStats.load
+
+import ebarrientos.deckStats.basics.Deck
+import zio.IO
+import zio.UIO
+import zio.Task
+import ebarrientos.deckStats.basics.DeckEntry
+
+/** Deck loader that loads a deck that looks like:
+  *
+  * 4x Swords to Plowshares
+  * Demonic Tutor
+  * ...
+  *
+  * Numbers can optionally be followed by 'x'. No number means a single copy
+  *
+  * @param text
+  * @param loader
+  */
+class NaturalDeckLoader(text: String, loader: CardLoader) extends DeckLoader {
+
+  override def load(): Task[Deck] =
+    Task
+      .collectAllPar(
+        text
+          .split("\n")
+          .toSeq
+          .map(parseLine _)
+          .map(parseCardDef _)
+      )
+      .map(_.flatten)
+      .map(des => Deck(des))
+
+  /** Split the line into the amount portion and the card name portion
+    *
+    * @param line the line to parse
+    * @return Amount and card name, if present
+    */
+  def parseLine(line: String): Option[(String, String)] = {
+    val regex = """\s*([0-9]*)\s*x?\s*(.+)""".r
+    regex
+      .findFirstMatchIn(line)
+      .map(m => (m.group(1), m.group(2)))
+  }
+
+  def parseCardDef(odef: Option[(String, String)]): Task[Option[DeckEntry]] =
+    odef
+      .map { case (copiesS, name) =>
+        if (name == "") Task.succeed(Option.empty)
+        else {
+          val amount =
+            if (copiesS == "") 1
+            else copiesS.toInt
+          loader.card(name).map(_.map(c => DeckEntry(c, amount)))
+        }
+      }
+      .getOrElse(Task.succeed(None))
+}
---
+++ core/src/test/scala/ebarrientos/deckStats/load/NaturalDeckLoaderTests.scala
@@ -0,0 +1,30 @@
+package ebarrientos.deckStats.load
+
+import utest.TestSuite
+import utest.Tests
+import utest._
+
+import ebarrientos.deckStats.DummyObjects
+import ebarrientos.deckStats.basics.DeckEntry
+
+object NaturalDeckLoaderTests extends TestSuite {
+  val d1 = """|4x Arthur Dent
+              | 4 Ford Prefect
+              |    2xZaphod BeebleBrox
+              |
+              |Tricia McMillan""".stripMargin
+
+  val tests = Tests {
+
+    test("load") {
+      val loader = new NaturalDeckLoader(d1, DummyObjects.dummyCardLoader)
+      val deck = zio.Runtime.default.unsafeRun(loader.load())
+
+      assert(deck.cards contains DeckEntry(DummyObjects.arthur, 4),
+             deck.cards contains DeckEntry(DummyObjects.ford, 4),
+             deck.cards contains DeckEntry(DummyObjects.trillian, 1),
+             deck.cards contains DeckEntry(DummyObjects.zaphod, 2))
+      deck.cards.size
+    }
+  }
+}
