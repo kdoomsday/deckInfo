40a61de4d397ea9106d463b9f7bf99f1c1a88fa7
Eduardo Barrientos <kdoomsday@gmail.com>
2023-03-02 10:11:49 -0400

    Test MagicIOLoader from a local json instead of making a call
    This makes tests faster and not dependant on the network

--- core/src/main/scala/ebarrientos/deckStats/load/MagicIOLoader.scala
+++ core/src/main/scala/ebarrientos/deckStats/load/MagicIOLoader.scala
@@ -20,68 +20,18 @@ object MagicIOLoader extends CardLoader with LoadUtils with URLUtils {
   private[this] val baseUrl = "https://api.magicthegathering.io/v1/cards"
 
   def card(name: String): Task[Option[Card]] = {
-    def getStr(value: JValue): String = value match {
-      case JString(s) => s
-      case _          => ""
-    }
-    def getInt(value: JValue): Int    = value match {
-      case JInt(num)                              => num.toInt
-      case JString(txt) if (txt matches "[0-9]+") => txt.toInt
-      case _                                      => 0
-    }
-
-    /** In an array of results, find the one that fully matches the expected name.
-      * This is important because the API finds partial matches (e.g. when
-      * searching for 'Wasteland' a result will come in for 'Wasteland Scorpion'
-      */
-    def findObject(arr: JArray): Option[JValue] = {
-      @tailrec
-      def fo(l: List[JValue]): Option[JValue] =
-        l match {
-          case jobject :: rest =>
-            if (getStr(jobject \ "name") == name)
-              Some(jobject)
-            else
-              fo(rest)
-
-          case Nil =>
-            None
-        }
-
-      fo(arr.arr)
-    }
-
-    // Construir la carta a partir del jobject correspondiente ya extraido de la lista
-    def cardFromJobject(j: JValue): Card = {
-      val manaCost: Seq[Mana] = parseAll(cost, getStr(j \\ "manaCost")).get
-      val (supertypes, types, subTypes) = parseTypesJson(j)
-      Card(
-        manaCost,
-        name,
-        types,
-        supertypes,
-        subTypes,
-        getStr(j \ "text"),
-        getInt(j \ "power"),
-        getInt(j \ "toughness")
-      )
-    }
-
     ZIO.attemptBlocking {
       log.info(s"Requesting card $name from MagicIOLoader")
-      val cardJsonResponse: Response = requests.get(
+      requests.get(
         baseUrl,
         params = Map("name" -> name),
         readTimeout = 20000,
         connectTimeout = 20000
       )
-
+    }
+    .flatMap { cardJsonResponse =>
       if (cardJsonResponse.statusCode == 200) {
-        (parse(cardJsonResponse.text) \\ "cards") match {
-          case arr @ JArray(_) =>
-            findObject(arr).map(cardFromJobject)
-          case _               => None
-        }
+        cardFromJsonString(name, cardJsonResponse.text())
       }
       else {
         throw new Exception(
@@ -110,5 +60,65 @@ object MagicIOLoader extends CardLoader with LoadUtils with URLUtils {
 
     log.debug(s"""Types = ${types.mkString(", ")}""")
     (supertypes.toSet, types.toSet, subtypes.toSet)
+  }
+
+  /** Given the card's json string, parse the card */
+  def cardFromJsonString(name: String, cardJson: String): Task[Option[Card]] = {
+    ZIO.attempt {
+      (parse(cardJson) \\ "cards") match {
+        case arr @ JArray(_) =>
+          findObject(name, arr).map(ojValue => cardFromJobject(name, ojValue))
+        case _               => None
+      }
+    }
+  }
+
+  /** In an array of results, find the one that fully matches the expected name.
+   * This is important because the API finds partial matches (e.g. when
+   * searching for 'Wasteland' a result will come in for 'Wasteland Scorpion'
+   */
+  private def findObject(name: String, arr: JArray): Option[JValue] = {
+    @tailrec
+    def fo(l: List[JValue]): Option[JValue] =
+      l match {
+        case jobject :: rest =>
+          if (getStr(jobject \ "name") == name)
+            Some(jobject)
+          else
+            fo(rest)
+
+        case Nil =>
+          None
+      }
+
+    fo(arr.arr)
+  }
+
+  // Construir la carta a partir del jobject correspondiente ya extraido de la lista
+  private def cardFromJobject(name: String, j: JValue): Card = {
+    val manaCost: Seq[Mana] = parseAll(cost, getStr(j \\ "manaCost")).get
+    val (supertypes, types, subTypes) = parseTypesJson(j)
+    Card(
+      manaCost,
+      name,
+      types,
+      supertypes,
+      subTypes,
+      getStr(j \ "text"),
+      getInt(j \ "power"),
+      getInt(j \ "toughness")
+    )
+  }
+
+
+  private def getStr(value: JValue): String = value match {
+    case JString(s) => s
+    case _          => ""
+  }
+
+  private def getInt(value: JValue): Int    = value match {
+    case JInt(num)                              => num.toInt
+    case JString(txt) if (txt matches "[0-9]+") => txt.toInt
+    case _                                      => 0
   }
 }
--- core/src/test/scala/ebarrientos/deckStats/load/MagicIOLoaderTest.scala
+++ core/src/test/scala/ebarrientos/deckStats/load/MagicIOLoaderTest.scala
@@ -5,31 +5,35 @@ import ebarrientos.deckStats.basics.Creature
 import ebarrientos.deckStats.TestHelper
 import org.slf4j.LoggerFactory
 import zio.Unsafe
+import java.nio.file.Paths
+import zio.ZIO
 
 object MagicIOLoaderTest extends TestSuite {
   val log = LoggerFactory.getLogger(getClass())
 
   val tests = Tests {
-    "Load a card successfully" - {
-      val ioDC = MagicIOLoader.card("Dark Confidant")
-      val t    = ioDC.map(oc =>
-        oc match {
-          case Some(dc) => {
-            val typesStr = dc.types.mkString(", ")
-            log.debug(s"Name=${dc.name}. Types=$typesStr")
+    "load a card from its json" - {
+      val path = "core/src/test/resources/MagicIOResponse.json"
+      val res =
+        ZIO.scoped(
+          ZIO.fromAutoCloseable(ZIO.attempt(scala.io.Source.fromFile(Paths.get(path).toFile())))
+            .flatMap { src =>
+              val cardJsonStr = src.getLines().mkString
+              MagicIOLoader.cardFromJsonString(name = "Dark Confidant", cardJson = cardJsonStr)
+            }
+            .map {
+              case Some(card) =>
+                assert(card.power == 2)
+                assert(card.toughness == 1)
+                assert(card.text.startsWith("At the beginning"))
+                assert(card.types.contains(Creature))
+                assert(card.subtypes contains "Human")
+                assert(card.subtypes contains "Wizard")
 
-            assert(dc.power == 2)
-            assert(dc.toughness == 1)
-            assert(dc.text.startsWith("At the beginning"))
-            assert(dc.types.contains(Creature))
-            assert(dc.subtypes contains "Human")
-            assert(dc.subtypes contains "Wizard")
-          }
-          case None     => throw new Exception("No card found")
-        }
-      )
-
-      TestHelper.run(t)
+              case None => throw new Exception("Did not parse card")
+            }
+        )
+      TestHelper.run(res)
     }
   }
 }
