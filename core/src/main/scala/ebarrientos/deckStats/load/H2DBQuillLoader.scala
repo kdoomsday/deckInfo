5804bb3c98b4b43172bc68b7c500396082d40435
Eduardo Barrientos <kdoomsday@gmail.com>
2023-03-09 11:05:24 -0400

    Make H2DBQuillLoader load multiple cards at once

--- core/src/main/scala/ebarrientos/deckStats/load/H2DBQuillLoader.scala
+++ core/src/main/scala/ebarrientos/deckStats/load/H2DBQuillLoader.scala
@@ -34,15 +34,18 @@ import ebarrientos.deckStats.stringParsing.MtgJsonParser
 
 /** Card loader using quill to handle database queries
   *
-  * @param helper [[CardLoader]] to fetch info if not present in this one
-  * @param ds [[DataSource]] to use
-  * @param runner [[ZioRunner]] which will be used to ensure tables are initialized
+  * @param helper
+  *   [[CardLoader]] to fetch info if not present in this one
+  * @param ds
+  *   [[DataSource]] to use
+  * @param runner
+  *   [[ZioRunner]] which will be used to ensure tables are initialized
   */
 class H2DBQuillLoader(val helper: CardLoader, ds: DataSource, runner: ZioRunner)
     extends CardLoader
     with LoadUtils {
 
-  private val log = LoggerFactory.getLogger(getClass())
+  private val log    = LoggerFactory.getLogger(getClass())
   private val parser = MtgJsonParser
 
   /** DataSource layer for the dao */
@@ -54,37 +57,37 @@ class H2DBQuillLoader(val helper: CardLoader, ds: DataSource, runner: ZioRunner)
 
   // Encoders/Decoders
   // Mana
-  implicit private val manaDecoder: MappedEncoding[String,Seq[Mana]] =
+  implicit private val manaDecoder: MappedEncoding[String, Seq[Mana]] =
     MappedEncoding[String, Seq[Mana]](costStr => parser.parseAll(parser.cost, costStr).get)
 
-  implicit private val manaEncoder: MappedEncoding[Seq[Mana],String] =
+  implicit private val manaEncoder: MappedEncoding[Seq[Mana], String] =
     MappedEncoding[Seq[Mana], String](cost => parser.stringify(cost))
 
   // Card types
-  implicit private val cardTypeDecoder: MappedEncoding[String,Set[CardType]] =
+  implicit private val cardTypeDecoder: MappedEncoding[String, Set[CardType]] =
     MappedEncoding[String, Set[CardType]](
       _.split(" ").map(CardType.apply).toSet
     )
 
-  implicit private val cardTypeEncoder: MappedEncoding[Set[CardType],String] =
+  implicit private val cardTypeEncoder: MappedEncoding[Set[CardType], String] =
     MappedEncoding[Set[CardType], String](_.mkString(" "))
 
   // Supertypes
-  implicit private val superTypeDecoder: MappedEncoding[String,Set[Supertype]] =
+  implicit private val superTypeDecoder: MappedEncoding[String, Set[Supertype]] =
     MappedEncoding[String, Set[Supertype]](
       _.split(" ").collect {
         case st if st.nonEmpty => Supertype.apply(st)
       }.toSet
     )
 
-  implicit private val superTypeEncoder: MappedEncoding[Set[Supertype],String] =
+  implicit private val superTypeEncoder: MappedEncoding[Set[Supertype], String] =
     MappedEncoding[Set[Supertype], String](_.mkString(" "))
 
   // Subtypes
-  implicit private val subTypeDecoder: MappedEncoding[String,Set[String]] =
+  implicit private val subTypeDecoder: MappedEncoding[String, Set[String]] =
     MappedEncoding[String, Set[String]](_.split(" ").toSet)
 
-  implicit private val subTypeEncoder: MappedEncoding[Set[String],String] =
+  implicit private val subTypeEncoder: MappedEncoding[Set[String], String] =
     MappedEncoding[Set[String], String](_.mkString(" "))
   // End Encoders/Decoders
 
@@ -124,6 +127,18 @@ class H2DBQuillLoader(val helper: CardLoader, ds: DataSource, runner: ZioRunner)
       .map(_ => ())
       .provide(dataSource)
 
+  /**
+   * Store multiple cards at once
+   *
+   * @param cards Cards to store
+   * @return Number of cards stores. Assumes all were inserted
+   */
+  private def storeMulti(cards: Seq[Card]): Task[Long] =
+    ctx
+      .run(liftQuery(cards).foreach(c => quote(query[Card].insertValue(c))))
+      .map(_ => cards.size.toLong)
+      .provide(dataSource)
+
   /** Attempt to run table creation */
   private def runInitScripts(): Task[Unit] = {
     def runSingle(path: Path): ZIO[DataSource, Throwable, Unit] = {
@@ -143,6 +158,34 @@ class H2DBQuillLoader(val helper: CardLoader, ds: DataSource, runner: ZioRunner)
       .provide(dataSource)
       .unit
   }
+
+  /** Queries for all cards at once, without resorting to the helper
+    *
+    * @param names
+    *   All the looked for cards
+    * @return
+    *   All of the cards that were found. This might be less than the requested cards
+    */
+  private def queryMultiple(names: Seq[String]): Task[Seq[Card]] = {
+    val q = quote {
+      query[Card]
+        .filter(c => liftQuery(names).contains(c.name))
+    }
+
+    ctx
+      .run(q)
+      .tap(res => ZIO.succeed(log.debug("Query found {} cards", res.size)))
+      .provide(dataSource)
+  }
+
+  /** Implement {{cards(names)}} so we can leverage querying multiple cards at once */
+  override def cards(names: Seq[String]): ZIO[Any, Throwable, Seq[Card]] =
+    for {
+      storedCards  <- queryMultiple(names)
+      missingNames <- ZIO.succeed(names.diff(storedCards.map(_.name)))
+      helperCards  <- helper.cards(missingNames)
+      _            <- storeMulti(helperCards)
+    } yield storedCards ++ helperCards
 
   /* Initialize the tables if necessary */
   runner.run(runInitScripts())
