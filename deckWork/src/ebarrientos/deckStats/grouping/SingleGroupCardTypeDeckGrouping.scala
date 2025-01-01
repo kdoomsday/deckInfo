a6edef7f1682e7a0bd38e8121d1662bc7a166713
Eduardo Barrientos <kdoomsday@gmail.com>
2025-01-01 11:10:33 -0400

    Use fold and mutable collections for a bit of efficiency

--- deckWork/src/ebarrientos/deckStats/grouping/SingleGroupCardTypeDeckGrouping.scala
+++ deckWork/src/ebarrientos/deckStats/grouping/SingleGroupCardTypeDeckGrouping.scala
@@ -3,7 +3,10 @@ package ebarrientos.deckStats.grouping
 import ebarrientos.deckStats.basics.Deck
 import ebarrientos.deckStats.basics.Card
 import ebarrientos.deckStats.basics.DeckEntry
+import ebarrientos.deckStats.basics.CardType.*
+import scala.collection.mutable.{Seq => MSeq}
 import ebarrientos.deckStats.basics.CardType
+import scala.meta.contrib.DocToken.GroupName
 
 /**
  * DeckGrouping that works on a card type basis and ensures each card is in a
@@ -11,24 +14,25 @@ import ebarrientos.deckStats.basics.CardType
  */
 class SingleGroupCardTypeDeckGrouping() extends DeckGrouping {
 
+  /** Helper to go from card type to GroupName in fewer characters */
+  private inline def s(ct: CardType): GroupName = ct.toString()
+
   override def group(deck: Deck): Map[GroupName, Seq[DeckEntry]] =
-    val (creatures, noncreature)       = deck.cards.partition(_.card.is(CardType.Creature))
-    val (planeswalkers, nonPW)         = noncreature.partition(_.card.is(CardType.Planeswalker))
-    val (artifacts, nonArtifact)       = nonPW.partition(_.card.is(CardType.Artifact))
-    val (enchantments, nonEnchantment) = nonArtifact.partition(_.card.is(CardType.Enchantment))
-    val (instants, nonInstant)         = nonEnchantment.partition(_.card.is(CardType.Instant))
-    val (sorceries, nonSorcery)        = nonInstant.partition(_.card.is(CardType.Sorcery))
-    val (lands, other)                 = nonSorcery.partition(_.card.is(CardType.Land))
-    Map(
-      CardType.Creature.toString()     -> creatures,
-      CardType.Planeswalker.toString() -> planeswalkers,
-      CardType.Artifact.toString()     -> artifacts,
-      CardType.Enchantment.toString()  -> enchantments,
-      CardType.Instant.toString()      -> instants,
-      CardType.Sorcery.toString()      -> sorceries,
-      CardType.Land.toString()         -> lands,
-      "Other"                          -> other
-    ).withDefaultValue(Seq.empty)
+    val initial = Map.empty[GroupName, MSeq[DeckEntry]].withDefaultValue(MSeq.empty[DeckEntry])
+    deck
+      .cards
+      .foldLeft(initial) {
+        case (acc, de) if de.card.is(Creature)     => acc + (s(Creature) -> acc(s(Creature)).appended(de))
+        case (acc, de) if de.card.is(Planeswalker) => acc + (s(Planeswalker) -> acc(s(Planeswalker)).appended(de))
+        case (acc, de) if de.card.is(Artifact)     => acc + (s(Artifact) -> acc(s(Artifact)).appended(de))
+        case (acc, de) if de.card.is(Enchantment)  => acc + (s(Enchantment) -> acc(s(Enchantment)).appended(de))
+        case (acc, de) if de.card.is(Instant)      => acc + (s(Instant) -> acc(s(Instant)).appended(de))
+        case (acc, de) if de.card.is(Sorcery)      => acc + (s(Sorcery) -> acc(s(Sorcery)).appended(de))
+        case (acc, de) if de.card.is(Land)         => acc + (s(Land)    -> acc(s(Land)).appended(de))
+        case (acc, de)                             => acc + ("Other"    -> acc("Other").appended(de))
+      }
+      .map((k, mutableV) => (k, mutableV.toSeq))
+      .withDefaultValue(Seq.empty) // Need this again because it gets lost after the map
 }
 
 object SingleGroupCardTypeDeckGrouping {
