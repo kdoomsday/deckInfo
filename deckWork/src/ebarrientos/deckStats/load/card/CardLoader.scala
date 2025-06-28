71ebfa72ed828c340f6aa4ee9524dc311060ae7b
Eduardo Barrientos <kdoomsday@gmail.com>
2025-06-27 23:17:48 -0400

    Fix docs for CardLoader

--- deckWork/src/ebarrientos/deckStats/load/card/CardLoader.scala
+++ deckWork/src/ebarrientos/deckStats/load/card/CardLoader.scala
@@ -1,11 +1,14 @@
 package ebarrientos.deckStats.load.card
 
+
 import ebarrientos.deckStats.basics.Card
 import zio.Task
 import zio.ZIO
 
+
 /** All card loaders must implement these methods. */
 trait CardLoader {
+
   /**
    * Load info of a single card by name
    *
@@ -14,22 +17,26 @@ trait CardLoader {
    */
   def card(name: String): Task[Option[Card]]
 
+
   /**
-   * Load several cards at once.
+   * Load several cards at once. All cards not found will not be present in
+   * the result and should not, as per contract.
    *
-   * The default implementation calls out to {{{card()}}}. Subclasses may choose
-   * to override this function
+   * The default implementation calls out to `card()` for each name.
+   * Subclasses may choose to override this function, in particular if they can
+   * do better than simply calling each card at a time
    *
-   * @param names Names to look for
-   * @return {{{Seq[Card]}}}
-   * All cards not found will not be present in the result
+   * @param names Names of cards to look for
+   * @return `Seq[Card]` with the fetched cards
    */
-  def cards(names: Seq[String]): ZIO[Any,Throwable,Seq[Card]] =
+  def cards(names: Seq[String]): ZIO[Any, Throwable, Seq[Card]] =
     for {
       cs <- ZIO.collectAllSuccesses(names.map(card))
     } yield cs.flatten
 
+
   /** Load cards by name. Utility present for usability purposes */
-  def cards(name: String, names: String*): ZIO[Any,Throwable,Seq[Card]] =
+  def cards(name: String, names: String*): ZIO[Any, Throwable, Seq[Card]] =
     cards(name +: names)
+
 }
