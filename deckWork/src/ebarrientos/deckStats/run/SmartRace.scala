e36ed81f46ec85dd6e0808acbb10946a89a61930
Eduardo Barrientos <kdoomsday@gmail.com>
2026-04-21 12:43:20 -0400

    If any side returns None the effect does not fail

--- deckWork/src/ebarrientos/deckStats/run/SmartRace.scala
+++ deckWork/src/ebarrientos/deckStats/run/SmartRace.scala
@@ -7,6 +7,7 @@ import java.{util => ju}
 
 object SmartRace {
   private case object NoneFailure
+  private case object StartedFailure
 
 
   /**
@@ -26,13 +27,27 @@ object SmartRace {
    * @param startDelay How long to wait for z1 before starting z2
    * @return The fetched result.
    */
-  def race[R, E1, E2, A](z1: ZIO[R, E1, Option[A]], z2: ZIO[R, E2, Option[A]])(
+  def raceSome[R, E1, E2, A](z1: ZIO[R, E1, Option[A]], z2: ZIO[R, E2, Option[A]])(
       startDelay: Duration
   ): ZIO[R, E1 | E2, Option[A]] =
-      val lside = z1.flatMap:
-          case Some(value) => ZIO.some(value)
-          case None        => z2
-      val rside = ZIO.sleep(startDelay) *> z2
-      lside.race(rside)
+    Ref.make(false).flatMap { z2Started =>
+        val z2Once = z2Started.modify { started =>
+          if (started) (ZIO.fail(StartedFailure), true)
+          else (z2, true)
+        }.flatten
+
+        val lside = z1
+          .flatMap:
+              case Some(value) => ZIO.some(value)
+              case None        => z2Once.catchAll(_ => ZIO.none) // If already none, failures become None
+          .catchAll(_ => z2Once) // Prevents fast z1 failure and z2 None from becoming failure
+        val rside = (ZIO.sleep(startDelay) *> z2Once.someOrFail(NoneFailure)).map(Option.apply)
+
+        lside
+          .disconnect
+          .race(rside.disconnect)
+          .catchSome { case NoneFailure => ZIO.none } // It can't be StartFailure on both sides
+          .asInstanceOf[ZIO[R, E1 | E2, Option[A]]]
+    }
 
 }
--- deckWork/test/src/ebarrientos/deckStats/run/SmartRaceTest.scala
+++ deckWork/test/src/ebarrientos/deckStats/run/SmartRaceTest.scala
@@ -14,53 +14,69 @@ object SmartRaceTest extends TestSuite {
       val z2    = ZIO.sleep(100.millis) *> ZIO.succeed(Some(2))
       val delay = 50.millis
 
-      val res = TestHelper.run(SmartRace.race(z1, z2)(delay))
+      val res = TestHelper.run(SmartRace.raceSome(z1, z2)(delay))
       assert(res == Some(1))
     }
 
     test("z2 returns Some when z1 is slow, use it") {
-      val z1    = ZIO.sleep(100.millis) *> ZIO.succeed(Some(1))
-      val z2    = ZIO.sleep(10.millis) *> ZIO.succeed(Some(2))
+      val z1    = ZIO.sleep(100.millis) *> ZIO.some(1)
+      val z2    = ZIO.sleep(10.millis) *> ZIO.some(2)
       val delay = 50.millis
 
-      val res = TestHelper.run(SmartRace.race(z1, z2)(delay))
+      val res = TestHelper.run(SmartRace.raceSome(z1, z2)(delay))
       assert(res == Some(2))
     }
 
     test("z1 returns None, result is z2's") {
-      val z1    = ZIO.succeed(None)
-      val z2    = ZIO.succeed(Some(2))
+      val z1    = ZIO.none
+      val z2    = ZIO.some(2)
       val delay = 50.millis
 
-      val res = TestHelper.run(SmartRace.race(z1, z2)(delay))
+      val res = TestHelper.run(SmartRace.raceSome(z1, z2)(delay))
       assert(res == Some(2))
     }
 
     test("z1 fails fast, result is z2's") {
       val z1    = ZIO.fail(new RuntimeException("boom"))
-      val z2    = ZIO.succeed(Some(2))
+      val z2    = ZIO.some(2)
       val delay = 50.millis
 
-      val res = TestHelper.run(SmartRace.race(z1, z2)(delay))
+      val res = TestHelper.run(SmartRace.raceSome(z1, z2)(delay))
       assert(res == Some(2))
     }
 
     test("both return None, returns None") {
-      val z1    = ZIO.succeed(None)
-      val z2    = ZIO.succeed(None)
+      val z1    = ZIO.none
+      val z2    = ZIO.none
       val delay = 50.millis
 
-      val res = TestHelper.run(SmartRace.race(z1, z2)(delay))
+      val res = TestHelper.run(SmartRace.raceSome(z1, z2)(delay))
       assert(res.isEmpty)
     }
 
+    test("z1 fails, z2 is None, result is None") {
+      val z1    = ZIO.fail(new Exception("boom"))
+      val z2    = ZIO.none
+      val delay = 20.millis
+      val res   = TestHelper.run(SmartRace.raceSome(z1, z2)(delay))
+      assert(res == None)
+    }
+
+    test("z1 is None, z2 fails, result is None") {
+      val z1    = ZIO.none
+      val z2    = ZIO.fail(new Exception("boom"))
+      val delay = 10.millis
+      val res   = TestHelper.run(SmartRace.raceSome(z1, z2)(delay))
+      assert(res == None)
+    }
+
     test("both fail, fails the task") {
-      val z1    = ZIO.fail(new RuntimeException("boom1"))
-      val z2    = ZIO.fail(new RuntimeException("boom2"))
+      val z1    = ZIO.fail(new Exception("boom1"))
+      val z2    = ZIO.fail(new Exception("boom2"))
       val delay = 50.millis
 
-      val res = intercept[RuntimeException] {
-        TestHelper.run(SmartRace.race(z1, z2)(delay))
+      val res = intercept[Exception] {
+        TestHelper.run(SmartRace.raceSome(z1, z2)(delay))
       }
       assert(res.getMessage.contains("boom1") || res.getMessage.contains("boom2"))
     }
@@ -71,7 +87,7 @@ object SmartRaceTest extends TestSuite {
       val delay = 50.millis
 
       val res = intercept[E1 | E2] {
-        TestHelper.run(SmartRace.race(z1, z2)(delay))
+        TestHelper.run(SmartRace.raceSome(z1, z2)(delay))
       }
 
       res match {
@@ -82,23 +98,23 @@ object SmartRaceTest extends TestSuite {
 
     test("z1 returns Some, z2 never started") {
       var z2Started = false
-      val z1        = ZIO.succeed(Some(1))
+      val z1        = ZIO.some(1)
       val z2        = ZIO.succeed {
         z2Started = true
         Some(2)
       }
       val delay     = 50.millis
 
-      val res = TestHelper.run(SmartRace.race(z1, z2)(delay))
+      val res = TestHelper.run(SmartRace.raceSome(z1, z2)(delay))
       assert(res == Some(1))
       assert(!z2Started)
     }
 
     test("both slow but z1 still faster") {
-      val z1    = ZIO.sleep(20.millis) *> ZIO.succeed(Some(1))
-      val z2    = ZIO.sleep(50.millis) *> ZIO.succeed(Some(2))
+      val z1    = ZIO.sleep(20.millis) *> ZIO.some(1)
+      val z2    = ZIO.sleep(50.millis) *> ZIO.some(2)
       val delay = 5.millis
-      val res   = TestHelper.run(SmartRace.race(z1, z2)(delay))
+      val res   = TestHelper.run(SmartRace.raceSome(z1, z2)(delay))
       assert(res == Some(1))
     }
 
@@ -107,22 +123,52 @@ object SmartRaceTest extends TestSuite {
       val z1            = ZIO.none
       val z2            = ZIO.some(2)
       val delay         = 500.millis
-      val res           = TestHelper.run(SmartRace.race(z1, z2)(delay))
+      val res           = TestHelper.run(SmartRace.raceSome(z1, z2)(delay))
       assert(res == Some(2))
       val totalDuration = (java.lang.System.currentTimeMillis() - start).millis
       assert(totalDuration < delay)
+    }
+
+    test("z2 starts and returns none before z1 finishes with some, return z1's value") {
+      val z1    = ZIO.sleep(50.millis) *> ZIO.some(1)
+      val z2    = ZIO.none
+      val delay = 10.millis
+      assert(TestHelper.run(SmartRace.raceSome(z1, z2)(delay)) == Some(1))
     }
 
     test("z2 is called only once, even if z1 finds nothing") {
       val run         = Ref.make(0).flatMap { runs =>
           val z1    = ZIO.none
           val z2    = runs.getAndUpdate(_ + 1) *> ZIO.sleep(50.millis) *> ZIO.some(2)
-          val delay = 10.millis
-          (SmartRace.race(z1, z2)(delay)).zip(runs.get)
+          val delay = 25.millis
+          (SmartRace.raceSome(z1, z2)(delay)).zip(runs.get)
       }
       val (res, runs) = TestHelper.run(run)
       assert(res == Some(2))
       assert(runs == 1)
+    }
+
+    test("minitest - failure") {
+      val z1  = ZIO.fail(new Exception("boom"))
+      val z2  = ZIO.sleep(10.millis) *> ZIO.succeed(2)
+      val res = TestHelper.run(z1.race(z2))
+      assert(res == 2)
+    }
+
+    test("minitest - failure on slow path") {
+      val z1  = ZIO.succeed(1)
+      val z2  = ZIO.sleep(40.millis) *> ZIO.fail(new Exception("boom 2"))
+      val res = TestHelper.run(z1.race(z2))
+      assert(res == 1)
+    }
+
+    test("minitest - fail flatmap") {
+      val z = ZIO
+        .attempt[Int](throw new Exception("boom"))
+        .flatMap(i => ZIO.succeed(i + 1))
+      intercept[Exception] {
+        TestHelper.run(z)
+      }
     }
   }
 
